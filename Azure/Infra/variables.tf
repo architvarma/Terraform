eece4365186cb71a0a0d6924dc2e2d718e02f5f5
Archit Varma <varma.archit@gmail.com>
2023-12-01 15:40:53 +0530

    Create variables.tf

---
+++ Azure/Infra/variables.tf
@@ -0,0 +1,43 @@
+variable "service_plans" {
+    default = {}
+}
+
+variable "app_insights" {
+    default = {}
+}
+
+variable "app_services" {
+    default = {}
+}
+
+variable "app_configs" {
+    default = {}
+}
+
+variable "virtual_networks" {
+    default = {}
+}
+
+variable "subnets" {
+    default = {}
+}
+
+variable "private_endpoints" {
+    default = {}
+}
+
+variable "private_dns_zones" {
+    default = {}
+}
+
+variable "dnszone_vnet_links" {
+    default = {}
+}
+
+variable "mysql_servers" {
+    default = {}
+}
+
+variable "mysql_databases" {
+    default = {}
+}
